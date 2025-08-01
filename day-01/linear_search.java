ccf7b28f3998d09c8a0055ba5c35aa6d016fb6e9
Pavankumar B R <162352324+pavankumarbr411@users.noreply.github.com>
2025-08-01 22:08:22 +0530

    Update linear_search.java

---
+++ day-01/linear_search.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Target found at index i
+            }
+        }
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 3, 8, 4, 2};
+        int target = 4;
+
+        int result = linearSearch(nums, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
